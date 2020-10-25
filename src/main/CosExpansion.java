7ef773dd37ad196823b5b0fc1e2f005afb204b66
Roman Kokorin <k.r.007.015@gmail.com>
2020-10-25 22:25:21 +0300

    Test for part 1 completed

--- src/main/CosExpansion.java
+++ src/main/CosExpansion.java
@@ -3,7 +3,7 @@ package main;
 import java.util.stream.LongStream;
 
 public class CosExpansion {
-    private static final int n = 8;
+    private static final int n = 10;
 
     public static double expanse(double x) {
         x = ((x/Math.PI)%2)*Math.PI;
@@ -13,17 +13,20 @@ public class CosExpansion {
         double result = 0;
 
         for (int i = 0; i <= n; i++) {
+
             result += (Math.pow(-1, i) * Math.pow(x, 2*i)) /
                     factorial(2*i);
         }
 
         return result;
+
     }
-    public static int factorial( int a){
-        int result=1;
+    public static long factorial( int a){
+        long result=1;
         while( a>0 ){
         result= result*a;
         a--;
+
         }
         return result;
     }
--- src/test/CosExpansionTest.java
+++ src/test/CosExpansionTest.java
@@ -1,8 +1,86 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import main.CosExpansion;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
 
 class CosExpansionTest {
+    private static final double DELTA = 1e-6;
+    private static final double EPSILON = 1e-3;
+
+    @Test
+    void testZero() {
+        assertEquals(Math.cos(0), CosExpansion.expanse(0), DELTA);
+    }
+    @Test
+    void testZeroMinusEpsilon() {
+        assertEquals(Math.cos(-EPSILON), CosExpansion.expanse(-EPSILON), DELTA);
+    }
+    @Test
+    void testZeroPlusEpsilon() {
+        assertEquals(Math.cos(EPSILON), CosExpansion.expanse(EPSILON), DELTA);
+    }
+
+    @Test
+    void testMinusPI() {
+        assertEquals(Math.cos(-Math.PI), CosExpansion.expanse(-Math.PI), DELTA);
+    }
+    @Test
+    void testMinusPIMinusEpsilon() {
+        assertEquals(Math.cos(-Math.PI-EPSILON), CosExpansion.expanse(-Math.PI-EPSILON), DELTA);
+    }
+    @Test
+    void testMinusPIPlusEpsilon() {
+        assertEquals(Math.cos(-Math.PI+EPSILON), CosExpansion.expanse(-Math.PI+EPSILON), DELTA);
+    }
+
+    @Test
+    void testPI() {
+        assertEquals(Math.cos(Math.PI), CosExpansion.expanse(Math.PI), DELTA);
+    }
+    @Test
+    void testPIMinusEpsilon() {
+        assertEquals(Math.cos(Math.PI-EPSILON), CosExpansion.expanse(Math.PI-EPSILON), DELTA);
+    }
+    @Test
+    void testPIPlusEpsilon() {
+        assertEquals(Math.cos(Math.PI+EPSILON), CosExpansion.expanse(Math.PI+EPSILON), DELTA);
+    }
+
+    @Test
+    void testHalfPi() {
+        assertEquals(Math.cos(Math.PI/2), CosExpansion.expanse(Math.PI/2), DELTA);
+    }
+
+    @Test
+    void testMinusHalfPi() {
+        assertEquals(Math.cos(-Math.PI/2), CosExpansion.expanse(-Math.PI/2), DELTA);
+    }
+
+    @Test
+    void testMinusHalfOne() {
+        assertEquals(Math.cos(-0.5), CosExpansion.expanse(-0.5), DELTA);
+    }
+    @Test
+    void testQuoterOfPlusDecrease() {
+        assertEquals(Math.cos(1.04), CosExpansion.expanse(1.04), DELTA);
+    }
+    @Test
+    void testThirdQuoterOfPlusDecrease() {
+        assertEquals(Math.cos(2.09), CosExpansion.expanse(2.09), DELTA);
+    }
+    @Test
+    void testQuoterOfMinusIncrease() {
+        assertEquals(Math.cos(-1.04), CosExpansion.expanse(-1.04), DELTA);
+    }
+    @Test
+    void testThirdQuoterOfMinusIncrease() {
+        assertEquals(Math.cos(-2.09), CosExpansion.expanse(-2.09), DELTA);
+    }
 
 
 }
